28d668547dfae08f86f6bca44a059ad907a4e5dd
Jeff Griffith <jgriffith@yahoo-inc.com>
2016-04-16 09:31:50 -0400

    sleep

--- src/main/java/jbx/Concurrency.java
+++ src/main/java/jbx/Concurrency.java
@@ -7,4 +7,19 @@ public class Concurrency {
 	public static void go(Runnable task) {
 		ForkJoinPool.commonPool().submit(task);
 	}
+
+	/**
+	 * Sleep the specified number of milliseconds.
+	 * @param millis
+	 * @return Whether the sleep was interrupted.
+	 */
+	public static boolean sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+			return false;
+		}
+		catch (Exception e) {
+			return true;
+		}
+	}
 }
